4d2b52390ccd6cd0ecb855ca3214f23801cae6e2
MichaelCJHurst <mhurst93@live.co.uk>
2017-10-02 11:30:13 +0100

    Added files and updated ThunderBorg to python3

---
+++ MikeyMonsterRC.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding: Latin-1
+""" Makes the MonsterBorg remote controllable """
+import time
+import os
+import sys
+import pygame
+import Classes.ThunderBorg
+
+def main():
+	""" Run when the program starts """
+	# Redirect the output to standard error, to ignore some pygame errors
+	sys.stdout = sys.stderr
+
+if __name__ == "__main__":
+	main()
